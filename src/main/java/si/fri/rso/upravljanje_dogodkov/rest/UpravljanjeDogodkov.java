5b8626674e484e9459009a97fa86e9cbda3e3aee
kustrun <domen.balantic1@gmail.com>
2017-11-27 10:19:17 +0100

    Implemented service discovery.

--- src/main/java/si/fri/rso/upravljanje_dogodkov/rest/UpravljanjeDogodkov.java
+++ src/main/java/si/fri/rso/upravljanje_dogodkov/rest/UpravljanjeDogodkov.java
@@ -1,5 +1,6 @@
 package si.fri.rso.upravljanje_dogodkov.rest;
 
+import com.kumuluz.ee.discovery.annotations.DiscoverService;
 import si.fri.rso.upravljanje_dogodkov.config.RestEndpoints;
 import si.fri.rso.upravljanje_dogodkov.pojo.Dogodek;
 
@@ -8,6 +9,7 @@ import javax.inject.Inject;
 import javax.ws.rs.*;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.Date;
@@ -20,6 +22,10 @@ public class UpravljanjeDogodkov {
     @Inject
     private RestEndpoints restEndpoints;
 
+    @Inject
+    @DiscoverService(value = "katalog_dogodkov-service", version = "1.0.x", environment = "dev")
+    private WebTarget target;
+
     @GET
     public Response test() {
         return Response.ok(restEndpoints.getKatalogDogodkovUrl()).build();
@@ -28,21 +34,24 @@ public class UpravljanjeDogodkov {
     @PUT
     @Path("/{dogodekId}")
     public Response editEvent(@PathParam("dogodekId") int dogodekId) {
+        WebTarget service = target.path("v1/katalog_dogodkov");
 
         System.out.println("DELA");
+        System.out.println(service.getUri().toString());
 
-        Response katalogDogodkovResponse = ClientBuilder.newClient().target(restEndpoints.getKatalogDogodkovUrl())
-                .path("katalog_dogodkov").path(Integer.toString(dogodekId)).request().get();
+        Response katalogDogodkovResponse = ClientBuilder.newClient().target(service.getUri())
+                .path(Integer.toString(dogodekId)).request().get();
 
         if(!katalogDogodkovResponse.getStatusInfo().getFamily().equals(Response.Status.Family.SUCCESSFUL)) {
             return Response.status(Response.Status.NOT_FOUND).build();
         }
 
         Dogodek dogodek = katalogDogodkovResponse.readEntity(Dogodek.class);
+        dogodek.setNazivDogodka("Domen Balantic");
         dogodek.setDatumDogodka(new Date());
 
-        return ClientBuilder.newClient().target("http://localhost:8080/v1/")
-                .path("katalog_dogodkov").request().put(Entity.json(dogodek));
+        return ClientBuilder.newClient().target(service.getUri())
+                .request().put(Entity.json(dogodek));
 
     }
 
